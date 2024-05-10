3743ea975668193c4484743ede643f5acb0bfc67
JoseAngel222 <166529088+JoseAngel222@users.noreply.github.com>
2024-05-11 01:50:49 +0200

    Final update VentaMayorista.java
    changes in DescuentoClienteMayorista

--- VentaMayorista.java
+++ VentaMayorista.java
@@ -12,18 +12,26 @@ public class VentaMayorista extends Venta{
     public double obtenerDescuentoMayorista() {
         return VentaMayorista.DescuentoDeClienteMayorista(this);
     }
-    public static double DescuentoDeClienteMayorista (Venta venta){
-        venta.setDescuento(venta.getPrecio());
-        if((venta.getPrecio()<300)&&(venta.getPrecio()>=200)){
-            venta.setDescuento(venta.getPrecio()*0.1); //10% de descuento si su compra esta entre 200 y 300
-        } else if((venta.getPrecio()>=300)&(venta.getPrecio()<500)){
-            venta.setDescuento(venta.getPrecio()*0.15); //15% de descuento si su compra esta entre 300 y 500
-        }else if ((venta.getPrecio()>=500)&(venta.getPrecio()<800)){
-            venta.setDescuento(venta.getPrecio()*0.2); //20% de descuento si su compra esta entre 500 y 800
-        } else{ venta.setDescuento(0);}
-        System.out.println("Descuento mayorista aplicado exitosamente.");
-        venta.setPrecio(venta.getPrecio()-venta.getDescuento());
-        return venta.getDescuento();
-    }
-}
+    public static double DescuentoDeClienteMayorista(Venta venta){
+        double descuento = 0.0; // Inicializar el descuento como 0.0 por defecto
 
+        // Calcular el descuento basado en el precio total de la venta
+        double precioTotal = venta.getPrecio();
+        if (precioTotal >= 200 && precioTotal < 300) {
+            descuento = precioTotal * 0.1; // 10% de descuento si el precio está entre 200 y 300
+        } else if (precioTotal >= 300 && precioTotal < 500) {
+            descuento = precioTotal * 0.15; // 15% de descuento si el precio está entre 300 y 500
+        } else if (precioTotal >= 500 && precioTotal < 800) {
+            descuento = precioTotal * 0.2; // 20% de descuento si el precio está entre 500 y 800
+        }
+
+        // Aplicar el descuento a la venta
+        venta.setDescuento(descuento);
+        double precioConDescuento = precioTotal - descuento;
+        venta.setPrecio(precioConDescuento);
+
+        System.out.println("Descuento mayorista aplicado exitosamente.");
+        return descuento;
+    }
+
+}
